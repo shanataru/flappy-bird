fc9dbd0a7636de9db8f24555286032212f894a70
shanataru <shanataru@gmail.com>
2024-06-10 17:28:40 +0100

    touch event

--- config.lua
+++ config.lua
@@ -1,23 +1,12 @@
---
--- For more information on config.lua see the Project Configuration Guide at:
--- https://docs.coronalabs.com/guide/basics/configSettings
---
+local aspectRatio = display.pixelHeight / display.pixelWidth
+local width = 360
+local height = width * aspectRatio
 
-application =
-{
-	content =
-	{
-		width = 320,
-		height = 480, 
+application = {
+	content = {
+		width = width,
+		height = height,
 		scale = "letterbox",
 		fps = 60,
-		
-		--[[
-		imageSuffix =
-		{
-			    ["@2x"] = 2,
-			    ["@4x"] = 4,
-		},
-		--]]
-	},
+	}
 }
--- main.lua
+++ main.lua
@@ -3,5 +3,33 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
+local relayout = require("libs.relayout")
 
-local bird = display.newRect(100, 200, 20,20)
+-- layout
+local _W, _H, _CX, _CY = relayout._W, relayout._H, relayout._CX, relayout._CY
+
+local bird = display.newRect(100, _CY, 20,20)
+bird.fill = {1,1,0}
+bird.velocity = 0
+bird.gravity = 0.6
+
+-- variables
+
+
+-- Update
+local function update()
+    bird.velocity = bird.velocity - bird.gravity
+    bird.y = bird.y - bird.velocity
+end
+
+Runtime:addEventListener("enterFrame", update)
+
+--Touch
+
+local function touch(event)
+    if event.phase == "began" then
+        bird.velocity = 10
+    end
+end
+
+Runtime:addEventListener("touch", touch)
